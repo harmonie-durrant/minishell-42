f1ff46a9d777cc406ee0528f7a37df330a91734e
harmonie-durrant <harmoniedurrant@gmail.com>
2024-08-28 21:16:42 +0200

    Fix unset

--- src/builtins/builtins.c
+++ src/builtins/builtins.c
@@ -6,7 +6,7 @@
 /*   By: rbryento <rbryento@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 13:51:37 by rbryento          #+#    #+#             */
-/*   Updated: 2024/08/28 08:55:52 by rbryento         ###   ########.fr       */
+/*   Updated: 2024/08/28 16:55:07 by rbryento         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,10 @@ int	unset_builtin(t_minishell *mini_data, char **args)
 
 	i = 1;
 	if (!args[1])
+	{
 		write(STDERR_FILENO, "unset: not enough arguments\n", 29);
+		mini_data->exit_code = 1;
+	}
 	while (args[i])
 	{
 		unset_env(args[i], mini_data);
--- src/builtins/unset_env.c
+++ src/builtins/unset_env.c
@@ -6,7 +6,7 @@
 /*   By: rbryento <rbryento@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/25 12:01:59 by rbryento          #+#    #+#             */
-/*   Updated: 2024/08/25 15:38:56 by rbryento         ###   ########.fr       */
+/*   Updated: 2024/08/28 21:15:00 by rbryento         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,27 @@ int	unset_env(const char *name, t_minishell *ms_data)
 	int		i;
 	int		j;
 	char	**new_env;
+	char	**split_tmp;
+	char	*tmp;
 
+	if (!name || !ms_data->env)
+		return (1);
 	i = 0;
 	while (ms_data->env[i])
 		i++;
-	new_env = calloc(i, sizeof(char *));
+	tmp = get_env((char *)name, ms_data->env);
+	if (!tmp)
+		return (0);
+	free(tmp);
+	new_env = ft_calloc(i, sizeof(char *));
 	i = 0;
 	j = 0;
 	while (ms_data->env[i])
 	{
-		if (ft_strncmp(name, ms_data->env[i], ft_strlen(name)) != 0)
-		{
-			new_env[j] = ms_data->env[i];
-			j++;
-		}
+		split_tmp = ft_split(ms_data->env[i], '=');
+		if (ft_strncmp(split_tmp[0], name, ft_strlen(name)) != 0)
+			new_env[j++] = ft_strdup(ms_data->env[i]);
+		free_2d(split_tmp);
 		i++;
 	}
 	new_env[j] = NULL;
